6713d5cb4dba5d69a24981256e7ef1c3759afe32
Muxtar Zəkəriyəyəv <98341413+zekeriyeyevmuxtar@users.noreply.github.com>
2023-07-22 21:35:52 +0400

    Add files via upload

---
+++ Task 7/Case №3.py
@@ -0,0 +1,11 @@
+name= ['Muxtar', 'Merdan','Elmeddin']
+surname=['Zekeriyeyev', 'Haciyev', 'Qeribov']
+
+inputname=input('Adınzı daxil edin:')
+inputsurname=input('Soyadınızı daxil edin:')
+
+name.append(inputname)
+surname.append(inputsurname)
+
+print(name)
+print(surname)
