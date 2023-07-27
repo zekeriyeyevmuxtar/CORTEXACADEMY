ed875efd0474c10803857de978df030aeb393c62
Muxtar Zəkəriyəyəv <98341413+zekeriyeyevmuxtar@users.noreply.github.com>
2023-07-27 11:28:08 +0400

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,42 @@
+#ad = ""
+
+#while len(ad)==0:
+#    ad=input("Adiniz:")
+
+#while True:
+#    ad= input("adiniz: ")
+#    if(ad!=""):
+#        break
+
+
+'''x=1
+while (x<2000000000):
+    print(x)
+    x = x+1 #200000 qeder alt alta cap edecek'''
+
+'''# for döngüsü:
+
+liste=[1,2,3,4,5,6]
+
+for rakam in liste:
+    print(rakam)    # alt-alta listedeki reqemleri cap edir. '''
+
+
+'''for i in range(0,10): #range funksiyasi arasinda yazdigimiz reqem araliqlerin cap edir.amma diqqet edeceyimiz yer odurki sonuncu reqem daxil deyil.
+    print(i)'''
+
+'''for i in range(1,20,2): #burda range icinde yazilan 3-cw deyer stepi yeni nece nece sayacagini ve ya addimlayacagini bildirir.
+    print(i)'''
+
+'''for i in range(1,21):
+    if(i%2==0):
+        print(i) #ekrana cwt ededleri cigarmak ucun istifade edilir'''
+
+
+'''for i in range(1,21):
+    if(i%2!=0):
+        print(i) # burda ise beraber deyil operatorundan istifade ederek tek ededleri ekrana  cigardig'''
+
+
+
+
