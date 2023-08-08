d6d42fb3d9b3dae1ac697ca85ec6e24f85aa17ce
Muxtar Zəkəriyəyəv <98341413+zekeriyeyevmuxtar@users.noreply.github.com>
2023-08-08 16:14:42 +0400

    Add files via upload

---
+++ Task 8.2/Case1.py
@@ -0,0 +1,12 @@
+dic1={'Adiniz':'Muxtar', 'Soyadiniz':'Zekeriyeyev', 'Mailiniz':'zekeriyeyev123@gmail.com' }
+dic2={'Adiniz':'Vusal', 'Soyadiniz':'Heydarov', 'Mailiniz':'vusal_heydarov123@gmail.com' }
+dic3={'Adiniz':'Merdan', 'Soyadiniz':'Haciyev', 'Mailiniz':'hacyev.merdan.2004@gmail.com' }
+
+for key,value in dic1.items():
+    print('Istifadecinin'+' '+key+' '+value+'dir')
+
+for key,value in dic2.items():
+    print('Istifadecinin'+' '+key+' '+value+'dir')
+
+for key,value in dic3.items():
+    print('Istifadecinin'+' '+key+' '+value+'dir')
---
+++ Task 8.2/OOP.py
@@ -0,0 +1,24 @@
+ # Obiyekt yonlu proqramlasdirma (OOP)- funksiyalar, def' | call etmek
+
+'''def funksiyadadi(): #passiv veziyyetdedi
+    emeliyyatlar       
+
+funksiyadadi() # aktiv hali  '''
+
+
+def adiniz(): #passiv
+    
+    print('Muxtar')
+
+adiniz() #aktiv edildi
+
+def topla():
+    a=5
+    b=2
+    print(a+b)
+
+topla()
+
+def toplamaq(a,b):
+    print(a+b)
+toplamaq(5,2)     
