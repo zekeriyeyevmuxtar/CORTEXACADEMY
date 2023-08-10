dc6c19390c3667ba839fcc00f1f5b2cc2b6ff8d3
Muxtar Zəkəriyəyəv <98341413+zekeriyeyevmuxtar@users.noreply.github.com>
2023-08-10 11:57:37 +0400

    Update Case4.py

--- Task 8/Case4.py
+++ Task 8/Case4.py
@@ -1,18 +1,6 @@
-'''def funk1(*args):
-    toplam = sum(*args)
-    return toplam
 
-def funk2(cavab):
-    for i in range (cavab +1):
-        if i%2 ==0:
-            print(i)   
 
-ededler = [10,20,30,40,50]
-cavab_funk1=ededlerin_cemi(ededler)
-
-cut_ededlerin_capi(cavab_funk1)'''
-
-'''def topla_ededler(arr):
+def topla_ededler(arr):
     # List-in içərisindəki bütün ədədləri toplayan funksiya
     toplam = sum(arr)
     return toplam
@@ -28,7 +16,7 @@ ededler = [10, 20, 30, 40, 50]
 cavab_birinci_funksiyadan = topla_ededler(ededler)
 
 # İkinci funksiyanı çağıraraq birinci funksiyanın cavabını argument kimi veririk
-cut_ededleri_cap_et(cavab_birinci_funksiyadan)'''
+cut_ededleri_cap_et(cavab_birinci_funksiyadan)
 
 
 
@@ -38,21 +26,3 @@ cut_ededleri_cap_et(cavab_birinci_funksiyadan)'''
 
 
 
-
-
-def first(*args):
-    cem = 0
-    for i in args:
-        cem += i
-    return cem
-
-print(first(0,1,5,7,12,23,35,40,))
-
-def second():
-    massiv = list()
-    for i in range(0,124):
-        if (i % 2 == 0):
-            massiv.append(i)
-    return massiv
-
-print(second())
